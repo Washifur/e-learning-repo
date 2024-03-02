65c44d13400f4a952c56b8a95df4079f3ca8a29e
Triz Iconic <iulyssetresor@gmail.com>
2024-03-01 21:25:37 -0800

    frontend

---
+++ frontend/src/redux/Store.js
@@ -0,0 +1,25 @@
+import {configureStore} from "@reduxjs/toolkit";
+import { adminReducer } from "./reducers/adminReducer.js";
+import { courseReduce } from "./reducers/courseReducer.js";
+import { otherReducer } from "./reducers/otherReducer.js";
+import { profileReducer, subscriptionReducer, userReducer } from "./reducers/userReducer.js";
+
+
+
+const store = configureStore({
+    reducer: {
+        user: userReducer,
+        profile: profileReducer,
+        course: courseReduce,
+        subscription: subscriptionReducer,
+        admin: adminReducer,
+        other: otherReducer,
+    }
+})
+
+
+export default store;
+
+
+
+export const server= "http://localhost:3000/api/v1";
